0a139e626956e3a5e0dcffbaefd8e39e10bd3eca
venkateshwar <kamlekar.venkatesh@gmail.com>
2022-10-29 12:28:34 +0530

    refactor the code

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-function renderAndShowPDF(sourceElement, endCallback) {
-  var globalStyles = {
+(async () => {
+  const pdfDocumentObject = new jspdf.jsPDF("p", "pt", "a4");
+
+  const printStyleSettings = {
     marginLeft: 10,
     marginRight: 10,
     marginTop: 10,
@@ -9,62 +11,71 @@ function renderAndShowPDF(sourceElement, endCallback) {
     dpi: 300,
   };
 
-  function convertPdfFromHtml(doc, callback) {
-    const opt = {
-      margin: [
-        globalStyles.marginTop,
-        globalStyles.marginRight,
-        globalStyles.marginBottom,
-        globalStyles.marginLeft,
-      ],
-      enableLinks: true,
-      pagebreak: {
-        avoid: ["tr", ".submodule-block", ".preface-page", ".first-page"],
-        mode: ["css", "legacy"],
-      },
-      image: { type: "jpeg", quality: 1 },
-      html2canvas: {
-        allowTaint: true,
-        dpi: globalStyles.dpi,
-        letterRendering: true,
-        logging: false,
-        scale: globalStyles.scale,
-        scrollX: 0,
-        scrollY: 0,
-      },
-      jsPDF: { doc },
-    };
+  const printFormatOptions = {
+    margin: [
+      printStyleSettings.marginTop,
+      printStyleSettings.marginRight,
+      printStyleSettings.marginBottom,
+      printStyleSettings.marginLeft,
+    ],
+    enableLinks: true,
+    pagebreak: {
+      avoid: ["tr", ".submodule-block", ".preface-page", ".first-page"],
+      mode: ["css", "legacy"],
+    },
+    image: { type: "jpeg", quality: 1 },
+    html2canvas: {
+      allowTaint: true,
+      dpi: printStyleSettings.dpi,
+      letterRendering: true,
+      logging: false,
+      scale: printStyleSettings.scale,
+      scrollX: 0,
+      scrollY: 0,
+    },
+    jsPDF: { doc: pdfDocumentObject },
+  };
 
+  async function getPrintReadyDocumentObjectFromHtmlElement(htmlElement) {
     return html2pdf()
-      .from(sourceElement)
-      .set(opt)
+      .from(htmlElement)
+      .set(printFormatOptions)
       .toPdf()
       .get("pdf")
-      .then((pdf) => {
-        const totalPages = pdf.internal.getNumberOfPages();
-
-        for (let i = 1; i < totalPages + 1; i++) {
-          pdf.setPage(i);
-          pdf.setFontSize(globalStyles.pageNumberFontSize);
-          pdf.text(
-            `Page ${i} of ${totalPages}`,
-            globalStyles.marginLeft,
-            pdf.internal.pageSize.getHeight() - globalStyles.marginBottom / 2
-          );
-        }
-
-        callback(pdf);
+      .catch((err) => {
+        console.error(err);
       });
   }
 
-  function addPdfToIframe(doc) {
-    var iframe = document.createElement("iframe");
-    iframe.onload = function () {
-      endCallback(iframe);
-    };
-    iframe.setAttribute(
-      "style",
-      `
+  function addPageNumberToEachPage(printReadyDocumentObject) {
+    const totalPages = printReadyDocumentObject.internal.getNumberOfPages();
+    const pageHeight = printReadyDocumentObject.internal.pageSize.getHeight();
+    const pageNumberPlacementX = printStyleSettings.marginLeft;
+    const pageNumberPlacementY =
+      pageHeight - printStyleSettings.marginBottom / 2;
+
+    for (let pageNumber = 1; pageNumber < totalPages + 1; pageNumber++) {
+      printReadyDocumentObject.setPage(pageNumber);
+      printReadyDocumentObject.setFontSize(
+        printStyleSettings.pageNumberFontSize
+      );
+      printReadyDocumentObject.text(
+        `Page ${pageNumber} of ${totalPages}`,
+        pageNumberPlacementX,
+        pageNumberPlacementY
+      );
+    }
+  }
+
+  async function showPrintReadyPageInIframe(printReadyDocumentObject) {
+    return new Promise((resolve) => {
+      var iframe = document.createElement("iframe");
+      iframe.onload = function () {
+        resolve(iframe);
+      };
+      iframe.setAttribute(
+        "style",
+        `
         position:fixed;
         right:0; 
         top:0; 
@@ -74,22 +85,22 @@ function renderAndShowPDF(sourceElement, endCallback) {
         width: 100%;
         height: 100%;
       `
-    );
+      );
 
-    var blobPDF = new Blob([doc.output("blob")], { type: "application/pdf" });
-    var blobUrl = URL.createObjectURL(blobPDF);
-    iframe.src = blobUrl;
+      var blobPDF = new Blob([printReadyDocumentObject.output("blob")], {
+        type: "application/pdf",
+      });
+      var blobUrl = URL.createObjectURL(blobPDF);
+      iframe.src = blobUrl;
 
-    document.body.appendChild(iframe);
+      document.body.appendChild(iframe);
+    });
   }
 
-  var doc = new jspdf.jsPDF("p", "pt", "a4");
-
-  convertPdfFromHtml(doc, (pdf) => {
-    addPdfToIframe(pdf);
-  });
-}
-
-renderAndShowPDF(document.querySelector(".pdf_print_container"), () => {
-  // stop loader here
-});
+  const htmlContainer = document.querySelector(".pdf_print_container");
+  const printReadyDocumentObject =
+    await getPrintReadyDocumentObjectFromHtmlElement(htmlContainer);
+  addPageNumberToEachPage(printReadyDocumentObject);
+  await showPrintReadyPageInIframe(printReadyDocumentObject);
+  // hide loader and stuff
+})();
